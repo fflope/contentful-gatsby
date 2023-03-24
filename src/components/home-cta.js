debea202acf08795cf477b327396e0e79cf72f34
fflope <127733852+fflope@users.noreply.github.com>
2023-03-24 20:07:56 +0800

    Added CTA row

--- src/components/home-cta.js
+++ src/components/home-cta.js
@@ -1,21 +1,48 @@
 import React from 'react'
-import { GatsbyImage } from 'gatsby-plugin-image'
-import { StaticImage } from 'gatsby-plugin-image'
 
 import Container from '../components/container'
-import * as styles from './hero-custom.module.css'
+import * as styles from './home-cta.module.css'
 
-const HeroCustom = ({ image, title, content, buttonlabel, buttonlink }) => (
-  <div className={styles.outerContainer}>
-      <StaticImage alt="Unsplash image" className={styles.backgroundImage} style={{ position: "absolute" }} src="https://images.ctfassets.net/cdypw9nxi8dj/50cWH3Pk42Lp6K4GnLHKe/94e432d36f35029608fc328476ac4ba0/bwb-home-banner.jpg" />
+const HomeCta = ({ title, content }) => (
+
       <div className={styles.container}>
-      <h1 className={`content ${styles.heroCustomMinervaBold}`}>{title}</h1>
+      <h1 className={`content ${styles.homeCTAHeading}`}>Let's Make this as Easy as Possible</h1>
       {content && (
-        <div className={styles.heroCustomMinerva}>{content}</div>
+        <div className={styles.homeCTAText}>Family law matters can be complex. Here are some things that you should know about Best Wilson Buckley.</div>
       )}
-      <a href={buttonlink} className={styles.btnPrimary}>{buttonlabel}</a>
+      <div className={styles.homeCTABoxesRow}>
+        <div className={styles.homeCTABoxes}>
+          <div className={styles.homeCTABoxesInner}>
+            <p className={styles.InnerHeading}>Get Instant<br/>Answers Now</p>
+            <p className={styles.InnerText}>We know that family law issues have a tendency to keep you awake at night. Get instant answers now with your own customised map up and down the family law mountain.</p>
+            <div className={styles.IconText}>
+              Use our amazing AI driven technology to give you customised legal advice
+            </div>
+          </div>
+          <a href="/">START NOW</a>
+        </div>
+        <div className={styles.homeCTABoxes}>
+          <div className={styles.homeCTABoxesInner}>
+            <p className={styles.InnerHeading}>Get Instant<br/>Answers Now</p>
+            <p className={styles.InnerText}>We know that family law issues have a tendency to keep you awake at night. Get instant answers now with your own customised map up and down the family law mountain.</p>
+            <div className={styles.IconText}>
+              Use our amazing AI driven technology to give you customised legal advice
+            </div>
+          </div>
+          <a href="/">START NOW</a>
+        </div>
+        <div className={styles.homeCTABoxes}>
+          <div className={styles.homeCTABoxesInner}>
+            <p className={styles.InnerHeading}>Get Instant<br/>Answers Now</p>
+            <p className={styles.InnerText}>We know that family law issues have a tendency to keep you awake at night. Get instant answers now with your own customised map up and down the family law mountain.</p>
+            <div className={styles.IconText}>
+              Use our amazing AI driven technology to give you customised legal advice
+            </div>
+          </div>
+          <a href="/">START NOW</a>
+        </div>
       </div>
-    </div>
+      </div>
 )
 
-export default HeroCustom
+export default HomeCta
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,13 @@ import Layout from '../components/layout'
 import Hero from '../components/hero'
 import ArticlePreview from '../components/article-preview'
 import HeroCustom from '../components/hero-custom'
+import HomeCta from '../components/home-cta'
 
 class RootIndex extends React.Component {
   render() {
     const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
     const [author] = get(this, 'props.data.allContentfulPerson.nodes')
     const [herocustom] = get(this, 'props.data.allContentfulHeroCustom.nodes')
-    const [homecta] = get(this, 'props.data.allContentfulHeroCustom.nodes')
-
     return (
       <Layout location={this.props.location}>
         <HeroCustom
@@ -22,6 +21,10 @@ class RootIndex extends React.Component {
           content={herocustom.subHeading}
           buttonlabel={herocustom.buttonLabel}
           buttonlink={herocustom.buttonLink}
+        />
+        <HomeCta
+          title={herocustom.heading}
+          content={herocustom.subHeading}
         />
         <ArticlePreview posts={posts} />
       </Layout>
